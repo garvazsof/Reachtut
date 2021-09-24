39425162cc154023462430b8922a171ca1767ff1
garvazsof <garvazsof@gmail.com>
2021-09-24 10:28:41 -0500

    Bets and Wagers

--- index.mjs
+++ index.mjs
@@ -7,12 +7,16 @@ const stdlib = loadStdlib(process.env);
     const accAlice = await stdlib.newTestAccount(startingBalance);
     const accBob = await stdlib.newTestAccount(startingBalance);
 
+    const fmt = (x) => stdlib.formatCurrency(x, 4);
+    const getBalance = async (Who) => fmt(await stdlib.balanceOf(Who));
+    const beforeAlice = await getBalance(accAlice);
+    const beforeBob = await getBalance(accBob);
+
     const ctcAlice = accAlice.deploy(backend);
     const ctcBob = accBob.attach(backend, ctcAlice.getInfo());
 
     const HAND = ['Rock', 'Paper', 'Scissors'];
     const OUTCOME = ['Bob wins', 'Draw', 'Alice Wins'];
-
     const Player = (Who) => ({
         getHand: () => {
             const hand = Math.floor(Math.random() * 3);
@@ -27,9 +31,20 @@ const stdlib = loadStdlib(process.env);
     await Promise.all([
         backend.Alice(ctcAlice, {
             ...Player('Alice'),
+            wager: stdlib.parseCurrency(5),
         }),
         backend.Bob(ctcBob, {
             ...Player('Bob'),
+            acceptWager: (amt) => {
+                console.log(`Bob accepts the wager of ${fmt(amt)}.`);
+            },
         }),
     ]);
+
+    const afterAlice = await getBalance(accAlice);
+    const afterBob = await getBalance(accBob);
+
+    console.log(`Alice went from ${beforeAlice} to ${afterAlice}.`);
+    console.log(`Bob went from ${beforeBob} to ${afterBob}`);
+    
 })();
